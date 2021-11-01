3a67e64585ccd816e12d74ee19bee5007cef4ee8
Stephen <stevriss22@gmail.com>
2021-11-01 16:10:48 -0400

    almost done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+greet("steve")
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end 
+greet_with_default()
+
+def add int1, int2
+    
+    return int1 + int2
+    
+end
+
+add(1, 2)
+
+def halve(num) do
+    if num == str
+        nill
+    else
+        num/2
+    
+end
+    
+halve(6)
